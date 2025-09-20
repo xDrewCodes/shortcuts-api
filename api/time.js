e124239f6b964617b63bfbc32ae94524357f291e
Drew Allred <drew.e.allred@gmail.com>
2025-09-20 12:19:51 -0400

    endpoint method fix

--- api/time.js
+++ api/time.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
