24bee8fa84f13f044bdbc4c7949a9e4b368d960e
Drew Allred <drew.e.allred@gmail.com>
2025-09-20 12:18:26 -0400

    endpoint

--- api/time.js
+++ api/time.js
@@ -1,9 +1,8 @@
-export default async function time(req, res) {
-    if (req.method !== 'GET') {
-        return res.status(405).json({ error: 'Method Not Allowed' })
-    }
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
-    return res.status(200).json({
-        'success'
-    })
+  // Your logic here
+  return res.status(200).json({ success: true });
 }
