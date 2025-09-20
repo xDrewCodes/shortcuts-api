d22085e9d4e849caab3c4241d7749ed104e79d5f
Drew Allred <drew.e.allred@gmail.com>
2025-09-20 12:35:41 -0400

    time func

--- api/time.js
+++ api/time.js
@@ -1,8 +1,11 @@
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Method not allowed' })
   }
 
-  // Your logic here
-  return res.status(200).json({ success: true });
+  let currentTime = new Date().toISOString()
+
+  return res.status(200).json({ 
+    currentTime
+   })
 }
